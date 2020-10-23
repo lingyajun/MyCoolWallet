99388f70361baf81c7ef615b05263e3ac7b7c464
lingyajun <lingyajun@tutanota.com>
2020-10-23 16:06:10 +0800

    init xui
    配置第三方库

---
+++ app/src/main/java/com/bethel/mycoolwallet/CoolApplication.java
@@ -0,0 +1,24 @@
+package com.bethel.mycoolwallet;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
+
+import com.xuexiang.xui.XUI;
+
+public class CoolApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        XUI.init(this);
+        XUI.debug(true);
+    }
+}
--- app/src/main/java/com/bethel/mycoolwallet/activity/MainActivity.java
+++ app/src/main/java/com/bethel/mycoolwallet/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.bethel.mycoolwallet;
+package com.bethel.mycoolwallet.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.bethel.mycoolwallet.R;
 
 public class MainActivity extends AppCompatActivity {
 
